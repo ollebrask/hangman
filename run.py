782701fe30a0e89f2355296baca1e1acd1726b05
Olle Brask <ollstah@gmail.com>
2024-04-07 12:06:28 +0000

    Add choose difficulty menu

--- run.py
+++ run.py
@@ -18,7 +18,17 @@ def main():
     if menu_entry_index == 0:
         rules()
     elif menu_entry_index == 1:
-        print("Choose difficulty is under construction:)")
+        difficulty = ["1", "2", "3"]
+        terminal_menu = TerminalMenu(difficulty, title="Difficulty:")
+        menu_entry_index = terminal_menu.show()
+
+        if menu_entry_index == 0:
+            print("You choose difficulty 1")
+        elif menu_entry_index == 1:
+            print("You choose difficulty 2")
+        elif menu_entry_index == 2:
+            print("You choose difficulty 3")
+        
 
 
 main()
