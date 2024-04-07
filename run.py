5fe2af72510ec28b526c47b2ebce8f8983eb160d
Olle Brask <ollstah@gmail.com>
2024-04-07 11:21:35 +0000

    Add menu for game

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from simple_term_menu import TerminalMenu
+
+def main():
+    print("Welcome to Hangman game, please choose on of the following!")
+    options = ["Read rules", "Choose difficulty"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+
+
+main()
