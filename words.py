49a8463f1f127917e9fd945af71e33c0673eaee8
Olle Brask <ollstah@gmail.com>
2024-04-08 09:34:55 +0000

    Add file to store words

---
+++ words.py
@@ -0,0 +1,20 @@
+words_1 = [
+    "blue",
+    "horse",
+    "happy",
+    "green"
+    ]
+
+words_2 = [
+    "culture",
+    "villain",
+    "spaceship",
+    "marathon"
+    ]
+
+words_3 = [
+    "countryside",
+    "asparagus",
+    "vacancy",
+    "shuttlebus"
+    ]
