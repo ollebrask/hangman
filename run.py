e2eda932e7aafb1a6b845d4f9ace38f6d367e6f3
Olle Brask <ollstah@gmail.com>
2024-04-16 12:10:56 +0000

    Edit typo in rules

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ import random
 
 def rules():
     print("1. You can choose by 3 difficulties.")
-    print("'1' for easy, '2' for medium, '3' for hard")
+    print("   '1' for easy, '2' for medium, '3' for hard")
     print("2. If you guess a correct letter, the letter will appear in the word")
     print("3. If you guess all the correct letters in the word, you win!")
-    print("4. If you guess a incorrect letter, a body part is shown.")
-    print("5. If you guess 5 incorrect letters, you lose")
+    print("4. If you guess a incorrect letter, a part of hangmans body is shown.")
+    print("5. If you guess 5 incorrect letters, you lose\n")
     difficulty()
 
 def difficulty():
@@ -68,7 +68,7 @@ def game(word):
         print(f"Game Over. The word was: {word}")
 
 def main():
-    print("Welcome to Hangman game, please choose on of the following!\n")
+    print("Welcome to Hangman game, please choose one of the following:\n")
     options = ["Read rules", "Choose difficulty"]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
